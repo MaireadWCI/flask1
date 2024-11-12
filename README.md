af9f3d11da290e099c2a71a2a270ad4dccfa2b81
mairead white <mairead.white@students.codeinstitute.net>
2024-11-12 21:48:19 +0000

    initial commit create basic flask app

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 ![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
 
-Welcome USER_NAME,
+
+# pip3 install Flask
+# touch run.py /*creates new file run.py*/
+
+
+Welcome mairead white,
 
 This is the Code Institute student template for Gitpod. We have preinstalled all of the tools you need to get started. It's perfectly ok to use this template as the basis for your project submissions.
 
---
+++ run.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask  #importing our flask class, capital F indicates it is a class
+
+
+app = Flask(__name__)  #creates an instance of this class and storing in a veriable called "app"
+                    #the first argument of flask class is the name of applications module - our package (flack needs this to know where to look for templates and static files)
+
+@app.route("/")        #uses a route decorater to tell flask what URl to trigger the function that follows(decorator is a way of wrapping functions.all functions are objects and can be passed around)
+def index():           #when we try to browse the root directory, as indicated by the "/", then Flask triggers the index function underneath and returns the "Hello wold" text
+    return "Hello, World"
+
+
+if __name__ == "__main__": #if name is equal to main then we are goin to wrap our app with the following arguments
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),   #we are using the os module from teh stanard libary to get hte ip enviorment variable if it exists but set a default value if its not found
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True) #debug=true will allow us to debug our code much easier (NB should not be set to debug=true in a production application or when we submit our project for assessment)
+                    # as it allows arbitary code to run and this is a security flaw
