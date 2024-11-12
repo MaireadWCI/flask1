6f517c18b6538eaca735599a25ed0b0420ceb142
mairead white <mairead.white@students.codeinstitute.net>
2024-11-12 21:56:36 +0000

    create basic code for index.html and about.html and store in folder called templates

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 import os
-from flask import Flask  #importing our flask class, capital F indicates it is a class
+from flask import Flask, render_template  #importing our flask class, capital F indicates it is a class. render template help you avoid typing all your code into the python file
 
 
 app = Flask(__name__)  #creates an instance of this class and storing in a veriable called "app"
                     #the first argument of flask class is the name of applications module - our package (flack needs this to know where to look for templates and static files)
 
 @app.route("/")        #uses a route decorater to tell flask what URl to trigger the function that follows(decorator is a way of wrapping functions.all functions are objects and can be passed around)
-def index():           #when we try to browse the root directory, as indicated by the "/", then Flask triggers the index function underneath and returns the "Hello wold" text
-    return "Hello, World"
+def index():           #when we try to browse the root directory, as indicated by the "/", then Flask triggers the index function underneath and returns ....
+    return render_template("index.html")
 
 
 if __name__ == "__main__": #if name is equal to main then we are goin to wrap our app with the following arguments
